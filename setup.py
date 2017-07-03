06edd4a12edc0ebcf39e49cd2d1e79b69ae50212
Davide Gerosa <dgerosa@caltech.edu>
2017-07-02 19:05:40 -0700

    pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from distutils.core import setup
 
 setup(name = 'filltex',
-      version = '1.2',
-      description = 'A LaTeX reference tool',
-      long_description = 'A LaTeX reference tool',
-
+      version = '1.2.6',
+      description = 'Automatic queries to ADS and InSPIRE databases to fill LATEX bibliography',
+      long_description="See: `github.com/dgerosa/filltex <https://github.com/dgerosa/filltex>`_." ,
       author = 'Davide Gerosa and Michele Vallisneri',
       author_email = 'dgerosa@caltech.edu',
       url = 'https://github.com/dgerosa/filltex',
-
+      license='MIT',
       py_modules = ['fillbib'],
-
-      scripts = ['bin/fillbib','bin/filltex']
+      scripts = ['bin/fillbib','bin/filltex'],
+      include_package_data=True,
+      zip_safe=False
       )
